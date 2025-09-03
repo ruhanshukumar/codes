50b75b07feefa475beb4c8d5fc3ab0192555d774
ruhanshukumar <ruhanshukumar8@gmail.com>
2025-09-03 23:45:34 +0530

    Initial commit

---
+++ Python/1st program.py
@@ -0,0 +1,11 @@
+print("hello world")
+a=10
+b=50
+c=a+b
+print("sum of given numbers is:",c)
+d=int(b/a) 
+print("divison of numbers is:",d)
+a=int(input("enter first value"))
+b=int(input("enter second value"))
+c=(a+b)
+print("sum of values is",c)
